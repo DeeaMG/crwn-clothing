0b31e70e264f80e02ed56a1e5a1500c643772af0
DeeaMG <55800409+DeeaMG@users.noreply.github.com>
2022-07-04 01:10:16 +0300

    Update payment-form.component.jsx

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react';
-import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
-import { useSelector } from 'react-redux';
+import { useState } from "react";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { useSelector } from "react-redux";
 
-import { selectCartTotal } from '../../store/cart/cart.selector';
-import { selectCurrentUser } from '../../store/user/user.selector';
+import { selectCartTotal } from "../../store/cart/cart.selector";
+import { selectCurrentUser } from "../../store/user/user.selector";
 
-import { FormContainer } from './payment-form.styles';
-import { BUTTON_TYPE_CLASSES } from '../button/button.component';
+import { FormContainer } from "./payment-form.styles";
+import { BUTTON_TYPE_CLASSES } from "../button/button.component";
 
-import { PaymentButton, PaymentFormContainer } from './payment-form.styles';
+import { PaymentButton, PaymentFormContainer } from "./payment-form.styles";
 
 const PaymentForm = () => {
   const stripe = useStripe();
@@ -36,7 +36,7 @@ const PaymentForm = () => {
 
     const clientSecret = response.paymentIntent.client_secret;
 
-    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+    const paymentResult = await stripe.confirmCardPayment(clientSecret, {
       payment_method: {
         card: elements.getElement(CardElement),
         billing_details: {
@@ -50,8 +50,8 @@ const PaymentForm = () => {
     if (paymentResult.error) {
       alert(paymentResult.error.message);
     } else {
-      if (paymentResult.paymentIntent.status === 'succeeded') {
-        alert('Payment Successful!');
+      if (paymentResult.paymentIntent.status === "succeeded") {
+        alert("Payment Successful!");
       }
     }
   };
